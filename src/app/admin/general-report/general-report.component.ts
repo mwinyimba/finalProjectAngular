c004b9bea3d066185bdc2d44029a380f3ea20938
yussuf <mwinyimba>
2023-07-07 20:29:45 +0300

    final frontend

--- src/app/admin/general-report/general-report.component.ts
+++ src/app/admin/general-report/general-report.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./general-report.component.css']
 })
 export class GeneralReportComponent {
-
+constructor(){}
+printThisPage(){
+  window.print();
+}
 }
